aec72917f923282afb28c9c180590f1879e48981
Rafał Matyja <raf.matyja@gmail.com>
2024-06-12 21:06:10 +0200

    add handleMainNavigation class into main.js - first version of toggled mobile nav

---
+++ js/main.js
@@ -0,0 +1,22 @@
+class handleMainNavigation{
+    constructor(root){
+        this.mainNavElements = {
+            nav: root,
+            navList: document.querySelector(".nav__list"),
+            navItems: document.querySelectorAll(".nav__item"),
+            navLogo: document.querySelector(".nav__item--logo"),
+            navLinks: document.querySelectorAll(".nav__link"),
+        }
+
+        this.mainNavHamburgerBtn.addEventListener("click", () => this.toggleNav())
+    }
+    toggleNav(){
+        this.mainNavElements.nav.classList.toggle("nav--nav-expanded");
+        this.mainNavElements.navList.classList.toggle("nav__list--nav-expanded");
+        this.mainNavElements.navItems.forEach((item) => item.classList.toggle("nav__item--nav-expanded"));
+        this.mainNavElements.navLinks.forEach((item) => item.classList.toggle("nav__link--nav-expanded"));
+    }
+}
+
+const nav = document.querySelector(".nav")
+const mainnav = new handleMainNavigation(nav);
